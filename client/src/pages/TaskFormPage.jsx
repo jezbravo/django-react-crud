e054185e912ba84fae6b99861b4d9bd1f4c80df9
Ezequiel Bravo <126541104+jezbravo@users.noreply.github.com>
2023-12-13 23:30:17 -0300

    update TasksFormPage with form

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,6 +1,25 @@
 import React from "react"
+import { useForm } from 'react-hook-form'
+
 export function TaskFormPage() {
+
+  const { register, handleSubmit } = useForm()
+
+  const onSubmit = handleSubmit(data => {
+    console.log(data)
+  })
+
   return (
-    <div>Task Form Page</div>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="Title"
+          {...register("title", { required: true })}
+        />
+        <textarea rows="3" placeholder="Description"
+          {...register("description", { required: true })}
+        ></textarea>
+        <button>Save</button>
+      </form>
+    </div>
   )
 }

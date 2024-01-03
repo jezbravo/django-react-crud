b1285a94bb0736747b69380bdfdc7ef29d72e4a7
Ezequiel Bravo <126541104+jezbravo@users.noreply.github.com>
2024-01-02 23:24:17 -0300

    update tasks.api.js

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,5 +1,13 @@
-import axios from 'axios'
+import axios from "axios";
+
+const taskApi = axios.create({
+  baseURL: "http://localhost:8000/tasks/api/v1/tasks",
+});
 
 export const getAllTasks = () => {
-    return axios.get('http://localhost:8000/tasks/api/v1/tasks/')
-}
+  return taskApi.get("/");
+};
+
+export const createTask = (task) => {
+  return taskApi.post("/");
+};
